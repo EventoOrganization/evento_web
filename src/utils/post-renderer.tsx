217dfd9389d11be0282a52bf7f9aa246e2e10f11
DUFOURNET Franck <franckdufournet@hotmail.fr>
2024-08-16 15:09:02 +0700

    chore: comment out or remove obsolete files to fix TypeScript errors

--- src/utils/post-renderer.tsx
+++ src/utils/post-renderer.tsx
@@ -1,21 +1,21 @@
-import RichText from "../components/shared/RichText";
-import ImageSlider from "../components/ui/ImageSlider";
-import Quote from "../components/shared/Quote";
-import Media from "../components/ui/Media";
-import VideoEmbed from "../components/shared/VideoEmbed";
+// import Quote from "../components/shared/Quote";
+// import RichText from "../components/shared/RichText";
+// import VideoEmbed from "../components/shared/VideoEmbed";
+// import ImageSlider from "../components/ui/ImageSlider";
+// import Media from "../components/ui/Media";
 
-export function postRenderer(section: any, index: number) {
+export function postRenderer(section: any) {
   switch (section.__component) {
     case "shared.rich-text":
-      return <RichText key={index} data={section} />;
-    case "shared.slider":
-      return <ImageSlider key={index} data={section} />;
-    case "shared.quote":
-      return <Quote key={index} data={section} />;
-    case "shared.media":
-      return <Media key={index} data={section} />;
-    case "shared.video-embed":
-      return <VideoEmbed key={index} data={section} />;
+    //   return <RichText key={index} data={section} />;
+    // case "shared.slider":
+    //   return <ImageSlider key={index} data={section} />;
+    // case "shared.quote":
+    //   return <Quote key={index} data={section} />;
+    // case "shared.media":
+    //   return <Media key={index} data={section} />;
+    // case "shared.video-embed":
+    //   return <VideoEmbed key={index} data={section} />;
     default:
       return null;
   }
