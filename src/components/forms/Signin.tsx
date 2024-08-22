35ab674cb9ae613980c2e09c1456994e2174671d
vuduyhien1992 <vuduyhien1992@gmail.com>
2024-08-22 10:12:00 +0700

    fix page Signin

--- src/components/forms/Signin.tsx
+++ src/components/forms/Signin.tsx
@@ -53,64 +53,64 @@ export default function SignIn() {
 
   return (
     <>
-      <form action={dispatch}>
-        <div className="bg-white h-screen w-full">
-          <div className="p-9 max-w-lg mx-auto">
-            <div className="mt-16">
-              <BackButton />
+      <form action={dispatch} className="bg-white h-screen w-full">\
+        <div className="p-9 max-w-lg mx-auto">
+          <div className="mt-16">
+            <BackButton />
+          </div>
+          <div className="text-3xl font-bold mt-8">Sign in</div>
+          <Input
+            id="email"
+            type="email"
+            name="email"
+            placeholder="E-mail"
+            size="lg"
+            className="my-6"
+          />
+          <Input
+            placeholder="Password"
+            id="password"
+            size="lg"
+            className="my-6"
+            name="password"
+            endContent={
+              <button
+                className="focus:outline-none"
+                type="button"
+                onClick={toggleVisibility}
+                aria-label="toggle password visibility"
+              >
+                {isVisible ? <EyeSlashFilledIcon /> : <EyeFilledIcon />}
+              </button>
+            }
+            type={isVisible ? "text" : "password"}
+          />
+          <div className="flex justify-between items-center text-xs my-6">
+            <div className="flex-1 text-xs">
+              <Checkbox>
+                <div className="text-xs">Remember me</div>
+              </Checkbox>
             </div>
-            <div className="text-3xl font-bold mt-8">Sign in</div>
-            <Input
-              type="email"
-              label="E-mail"
-              className="my-6"
-              color="default"
-              id="email"
-              name="email"
-            />
-            <Input
-              label="Password"
-              id="password"
-              name="password"
-              endContent={
-                <button
-                  className="focus:outline-none"
-                  type="button"
-                  onClick={toggleVisibility}
-                  aria-label="toggle password visibility"
-                >
-                  {isVisible ? <EyeSlashFilledIcon /> : <EyeFilledIcon />}
-                </button>
-              }
-              type={isVisible ? "text" : "password"}
-            />
-            <div className="flex justify-between items-center text-xs my-6">
-              <div className="flex-1 text-xs">
-                <Checkbox>
-                  <div className="text-xs">Remember me</div>
-                </Checkbox>
-              </div>
-              <div className="text-right">Forgot password?</div>
+            <div className="text-right">Forgot password?</div>
+          </div>
+          <div className="flex justify-center">
+            <LoginButton />
+          </div>
+          <div className="flex justify-center mt-20 gap-10">
+            <div className="shadow-lg p-3 rounded-lg">
+              <GoogleIcon />
             </div>
-            <div className="flex justify-center">
-              <LoginButton />
+            <div className="shadow-lg p-3 rounded-lg">
+              <AppleIcon />
             </div>
-            <div className="flex justify-center mt-20 gap-10">
-              <div className="shadow-lg p-3 rounded-lg">
-                <GoogleIcon />
-              </div>
-              <div className="shadow-lg p-3 rounded-lg">
-                <AppleIcon />
-              </div>
-            </div>
-            <div>
-              <p className="text-center mt-20 text-md font-bold">
-                Don't have an account?
-                <Link href="#" className="ml-2">
-                  Sign Up
-                </Link>
-              </p>
-            </div>
+          </div>
+          <div>
+            <p className="text-center mt-20 text-md font-bold">
+              Don't have an account?
+              <Link href="/signup" className="ml-2">
+                Sign Up
+              </Link>
+            </p>
           </div>
         </div>
       </form>
