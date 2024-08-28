a1bde425d0fd0172f8572515c763077785324cd5
DUFOURNET Franck <franckdufournet@hotmail.fr>
2024-08-28 14:32:37 +0700

    feat: add session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/layout/Header";
 import Main from "@/components/layout/Main";
 import NavbarApp from "@/components/Navbar";
 import { Toaster } from "@/components/ui/sonner";
-import { UserProvider } from "@/contexts/authContext";
+import { SessionProvider } from "@/contexts/SessionProvider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -22,12 +22,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <body className={inter.className}>
-        <UserProvider>
+        <SessionProvider>
           <Toaster position="top-center" />
           <Header />
           <Main className="">{children}</Main>
           <NavbarApp />
-        </UserProvider>
+        </SessionProvider>
       </body>
     </html>
   );
---
+++ src/contexts/SessionProvider.tsx
@@ -0,0 +1,53 @@
+// src/contexts/AuthContext.tsx
+"use client";
+import { User } from "@/types/UserType";
+import React, { createContext, useContext, useState } from "react";
+
+interface SessionContextProps {
+  user: User | null;
+  token: string | null;
+  startSession: (user: User, token: string) => void;
+  endSession: () => void;
+  isAuthenticated: boolean;
+}
+
+const SessionContext = createContext<SessionContextProps | undefined>(
+  undefined,
+);
+
+export const SessionProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+
+  const startSession = (user: User, token: string) => {
+    setUser(user);
+    setToken(token);
+    document.cookie = `token=${token}; path=/;`;
+  };
+
+  const endSession = () => {
+    setUser(null);
+    setToken(null);
+    document.cookie = `token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+  };
+
+  const isAuthenticated = !!user && !!token;
+
+  return (
+    <SessionContext.Provider
+      value={{ user, token, startSession, endSession, isAuthenticated }}
+    >
+      {children}
+    </SessionContext.Provider>
+  );
+};
+
+export const useSession = () => {
+  const context = useContext(SessionContext);
+  if (!context) {
+    throw new Error("useSession must be used within a SessionProvider");
+  }
+  return context;
+};
