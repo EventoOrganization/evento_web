7d37718a8b2c35727c21e30f1771b7d94d71a307
DUFOURNET Franck <franckdufournet@hotmail.fr>
2024-08-29 15:01:06 +0700

    fix: remove testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,11 @@
 "use server";
 import Hero from "@/features/home/Hero";
 import HowItWorks from "@/features/home/HowItWorks";
-import Testimonials from "@/features/home/Testimonials";
 export default async function Home() {
-  const testimonials = [
-    {
-      // photo: "/path/to/photo1.jpg",
-      name: "Jane Doe",
-      quote: "This product changed my life!",
-      position: "CEO",
-      company: "TechCorp",
-    },
-    {
-      // photo: "/path/to/photo2.jpg",
-      name: "John Smith",
-      quote: "Outstanding service and quality.",
-      position: "CTO",
-      company: "InnovateX",
-    },
-    {
-      photo: "/path/to/photo3.jpg",
-      name: "Alice Johnson",
-      quote: "I would highly recommend this to anyone.",
-    },
-  ];
-
   return (
     <>
       <Hero />
       <HowItWorks />
-      <Testimonials testimonials={testimonials} />
     </>
   );
 }
