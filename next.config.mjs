010ea0640a4dd133f049dae8011647933693cf14
DUFOURNET Franck <franckdufournet@hotmail.fr>
2024-10-16 18:37:07 +0700

    fix: added rewrites for auth, user, events, and profile apis

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,30 @@ const nextConfig = {
       "github.com",
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/auth/(.*)", // Capture toutes les requêtes qui commencent par /auth/
+        destination: "http://backend.evento-app.io/auth/$1",
+      },
+      {
+        source: "/users/(.*)", // Capture toutes les requêtes qui commencent par /user/
+        destination: "http://backend.evento-app.io/user/$1",
+      },
+      {
+        source: "/events/(.*)", // Capture toutes les requêtes qui commencent par /events/
+        destination: "http://backend.evento-app.io/events/$1",
+      },
+      {
+        source: "/profile/(.*)", // Capture toutes les requêtes qui commencent par /profile/
+        destination: "http://backend.evento-app.io/profile/$1",
+      },
+      {
+        source: "/chats/(.*)", // Capture toutes les requêtes qui commencent par /profile/
+        destination: "http://backend.evento-app.io/chats/$1",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
