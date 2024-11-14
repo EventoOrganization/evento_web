c86dea1c27fa814868d0ced6c3e4ff677ae88c4b
DUFOURNET Franck <franckdufournet@hotmail.fr>
2024-11-14 15:39:04 +0700

    fix: add log to reach profile id page

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -9,12 +9,14 @@ type Props = {
 // Fonction pour récupérer les données côté serveur
 async function fetchProfileData(userId: string): Promise<any | null> {
   try {
+    console.log("Fetching profile for user:", userId);
     const profileRes = await fetchData<any>(
       `/profile/userProfile/${userId}`,
       HttpMethod.GET,
       null,
       null,
     );
+    console.log("Profile response:", profileRes);
     return profileRes.data || null;
   } catch (error) {
     console.error("Error fetching profile data:", error);
@@ -41,18 +43,22 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
 // Composant principal pour la page profil utilisateur (Server Component)
 export default async function UserProfilePage({ params }: Props) {
-  const profileData = await fetchProfileData(params.id);
+  try {
+    const profileData = await fetchProfileData(params.id);
+    if (!profileData) {
+      return <p>User not found.</p>;
+    }
 
-  if (!profileData) {
-    return <p>User not found.</p>;
+    return (
+      <UserProfile
+        profile={profileData}
+        upcomingEvents={profileData.upcomingEvents || []}
+        pastEvents={profileData.pastEvents || []}
+        hostingEvents={profileData.hostedEvents || []}
+      />
+    );
+  } catch (error) {
+    console.error("Error rendering UserProfilePage:", error);
+    return <p>Failed to load profile data.</p>;
   }
-
-  return (
-    <UserProfile
-      profile={profileData}
-      upcomingEvents={profileData.upcomingEvents || []}
-      pastEvents={profileData.pastEvents || []}
-      hostingEvents={profileData.hostedEvents || []}
-    />
-  );
 }
