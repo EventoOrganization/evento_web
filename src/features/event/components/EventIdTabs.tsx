7571239e8ef01bd2ef3307271bc8308a31b13302
DUFOURNET Franck <franckdufournet@hotmail.fr>
2024-12-04 08:47:49 +0700

    fix: no more cliping request modal

--- src/features/event/components/EventIdTabs.tsx
+++ src/features/event/components/EventIdTabs.tsx
@@ -1,4 +1,5 @@
 "use client";
+import EventoLoader from "@/components/EventoLoader";
 import Section from "@/components/layout/Section";
 import RenderMedia from "@/components/RenderMedia";
 import RequestModal from "@/components/RequestModal";
@@ -21,7 +22,6 @@ import Link from "next/link";
 import { useParams, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import DeleteEventButton from "./DeleteEventButton";
-import EventoLoader from "@/components/EventoLoader";
 export type EventStatusKeys = "isGoing" | "isFavourite" | "isRefused";
 
 const EventIdTabs = ({ evento }: { evento?: EventType }) => {
@@ -31,14 +31,15 @@ const EventIdTabs = ({ evento }: { evento?: EventType }) => {
   const { token, user } = useSession();
   const { toast } = useToast();
   const { users } = useGlobalStore((state) => state);
-  const [isRestricted, setIsRestricted] = useState(false);
   const [event, setEvent] = useState<EventType | null>(evento || null);
   const [selectedTab, setSelectedTab] = useState("Description");
   const [isAuthModalOpen, setIsAuthModalOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   // Access control state
   const [accessControl, setAccessControl] = useState({
+    isPublic: false,
     isPrivate: false,
+    isRestricted: false,
     isAdmin: false,
     isGuest: false,
     isTempGuest: false,
@@ -129,51 +130,78 @@ const EventIdTabs = ({ evento }: { evento?: EventType }) => {
     };
     fetchEventData();
   }, [eventId, token, toast]);
+
   // Calculer les droits d'accès
   useEffect(() => {
     setIsLoading(true);
+
     if (event) {
+      const isPublic = event.eventType === "public";
       const isPrivate = event.eventType === "private";
-      const isAdmin = event.user?._id === user?._id;
-      const isGuest =
-        event.guests?.some((guest) => guest._id === user?._id) || false;
-      const isTempGuest =
-        event.tempGuests?.some(
-          (guest) => guest.email === params.get("email"),
-        ) || false;
-      const hasAccess = !isPrivate || isAdmin || isGuest || isTempGuest;
-      setIsRestricted(event.restricted || false);
-      setAccessControl({ isPrivate, isAdmin, isGuest, isTempGuest, hasAccess });
+      const isRestricted = event.restricted || false;
+
+      const isAdmin = user?._id === event.user?._id;
+      const isGuest = user
+        ? event.guests?.some((guest) => guest._id === user._id) || false
+        : false;
+      const isTempGuest = user
+        ? event.tempGuests?.some(
+            (guest) => guest.email === params.get("email"),
+          ) || false
+        : false;
+
+      // La logique d'accès doit refléter les états décrits
+      const hasAccess =
+        isPublic ||
+        isAdmin ||
+        isGuest ||
+        isTempGuest ||
+        (!isRestricted && isPrivate);
+
+      setAccessControl({
+        isPublic,
+        isPrivate,
+        isRestricted,
+        isAdmin,
+        isGuest,
+        isTempGuest,
+        hasAccess,
+      });
+    } else {
+      setAccessControl((prev) => ({
+        ...prev,
+        hasAccess: false, // Si pas d'event ou user
+      }));
     }
+
     setIsLoading(false);
   }, [event, user, params]);
 
   // Affichage de l'événement
-  if (isLoading) {
+  if (isLoading || !event || !accessControl.hasAccess) {
     return (
       <div className="w-full h-full flex items-center justify-center">
         <EventoLoader />
       </div>
     );
   }
-  console.log("isRestricted", isRestricted);
+
   return (
     <>
+      {!accessControl.hasAccess && (
+        <RequestModal
+          isOpen={accessControl.isRestricted}
+          onRequestToJoin={handleRequestToJoin}
+          onAuthModalOpen={() => setIsAuthModalOpen(true)}
+          hasToken={!!token}
+        />
+      )}
       {event && (
         <>
-          {" "}
           <StructuredData event={event} />
           <div className="md:grid-cols-2 grid grid-cols-1 w-full max-w-7xl mx-auto">
             {isAuthModalOpen && (
               <AuthModal onAuthSuccess={() => setIsAuthModalOpen(false)} />
-            )}
-            {isRestricted && (
-              <RequestModal
-                isOpen={accessControl.isPrivate && !accessControl.hasAccess}
-                onRequestToJoin={handleRequestToJoin}
-                onAuthModalOpen={() => setIsAuthModalOpen(true)}
-                hasToken={!!token}
-              />
             )}
             <div className="md:p-10 md:pl-0 p-4 h-full ">
               <div className="flex items-center w-full justify-between mb-4">
@@ -250,6 +278,7 @@ const EventIdTabs = ({ evento }: { evento?: EventType }) => {
                   updateEventStatusLocally={updateEventStatusLocally}
                 />
               )}
+
               {selectedTab === "Attendees" && accessControl.hasAccess && (
                 <EventAttendeesTab
                   event={event}
