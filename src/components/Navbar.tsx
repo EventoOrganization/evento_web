be4ffe56db847f2f687eede0252fec7ef096dc1b
DUFOURNET Franck <franckdufournet@hotmail.fr>
2024-12-04 09:57:08 +0700

    feat: add navbar design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,20 +22,20 @@ export default function NavbarApp() {
     <nav
       key={pathname}
       className={cn(
-        "fixed w-full bg-background z-20 h-11 md:h-20 flex border justify-evenly md:min-w-72 md:py-4 items-center left-1/2 bottom-6 -translate-x-1/2 rounded-lg shadow max-w-80 md:w-full md:max-w-2xl",
+        "fixed w-full bg-background z-20 h-24  flex border-2 sm:rounded-t-xl justify-evenly md:min-w-72 md:py-4 items-center left-1/2 bottom-0 -translate-x-1/2  max-w-md",
         "transition-all duration-500 ease-in-out",
         isVisible ? "opacity-100" : "opacity-0 pointer-events-none",
       )}
     >
       <Link href="/" className="relative group">
-        <HomeIcon2 pathname={pathname === "/"} className="w-8 h-8" />
+        <HomeIcon2 pathname={pathname === "/"} className="w-6 h-6" />
         <span className="hidden md:block absolute left-1/2 border shadow -translate-x-1/2 bottom-full mb-2 px-2 py-1 bg-white text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap overflow-hidden text-ellipsis w-max">
           Back to home
         </span>
       </Link>
 
       <Link href="/discover" className="relative group">
-        <DiscoverIcon pathname={pathname === "/discover"} className="w-8 h-8" />
+        <DiscoverIcon pathname={pathname === "/discover"} className="w-6 h-6" />
         <p className="hidden md:block absolute left-1/2 border bg-white shadow -translate-x-1/2 bottom-full mb-2 px-2 py-1 text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap overflow-hidden text-ellipsis w-max">
           Discover events
         </p>
@@ -44,7 +44,7 @@ export default function NavbarApp() {
       <Link href="/create-event" className="relative group">
         <CreateEventIcon
           pathname={pathname === "/create-event"}
-          className="w-8 h-8"
+          className="w-6 h-6"
         />
         <span className="hidden md:block absolute left-1/2 border shadow -translate-x-1/2 bottom-full mb-2 px-2 py-1 bg-white text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap overflow-hidden text-ellipsis w-max">
           Create an event
@@ -52,14 +52,14 @@ export default function NavbarApp() {
       </Link>
 
       <Link href="/profile" className="relative group">
-        <ProfileIcon pathname={pathname === "/profile"} className="w-8 h-8" />
+        <ProfileIcon pathname={pathname === "/profile"} className="w-6 h-6" />
         <span className="hidden md:block absolute left-1/2 border shadow -translate-x-1/2 bottom-full mb-2 px-2 py-1 bg-white text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap overflow-hidden text-ellipsis w-max">
           Your profile
         </span>
       </Link>
 
       <Link href="/chats" className="relative group">
-        <TchatIcon pathname={pathname === "/chats"} className="w-8 h-8" />
+        <TchatIcon pathname={pathname === "/chats"} className="w-6 h-6" />
         <span className="hidden md:block absolute left-1/2 border shadow -translate-x-1/2 bottom-full mb-2 px-2 py-1 bg-white text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap overflow-hidden text-ellipsis w-max">
           Chat
         </span>
