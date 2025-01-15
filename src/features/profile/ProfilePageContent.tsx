aaa39d152de93e82aa2a28ee26b24f77b39846ac
DUFOURNET Franck <franckdufournet@hotmail.fr>
2025-01-15 11:43:31 +0700

    fix: use store in profile page

--- src/features/profile/ProfilePageContent.tsx
+++ src/features/profile/ProfilePageContent.tsx
@@ -4,48 +4,16 @@ import AuthModal from "@/features/auth/components/AuthModal";
 import StaticProfilePage from "@/features/profile/StaticProfilePage";
 import UserProfile from "@/features/profile/UserProfile";
 import { useGlobalStore } from "@/store/useGlobalStore";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 export default function ProfilePageContent() {
   const session = useSession();
   const [isAuthModalOpen, setIsAuthModalOpen] = useState(false);
   const globalStore = useGlobalStore();
   const { events, userInfo } = useGlobalStore((state) => state);
-  // const [userInfo, setUserInfo] = useState<UserType | null>(null);
-  // const upcomingFilteredEvents = useMemo(() => {
-  //   return events.filter((event) => event.isGoing || event.isFavourite);
-  // }, [events]);
-  const upcomingFilteredEvents = events.filter(
-    (event) => event.isGoing || event.isFavourite,
-  );
-  console.log(
-    "eventupcoming",
-    events.filter((event) => event.isGoing || event.isFavourite),
-  );
-  // const loadUser = async (token: string) => {
-  //   try {
-  //     const userRes = await fetchData(
-  //       `/profile/getLoggedUserProfile`,
-  //       HttpMethod.GET,
-  //       null,
-  //       token,
-  //     );
-  //     if (userRes && !userRes.error && userRes.data) {
-  //       setUserInfo(userRes.data as UserType);
-  //     } else {
-  //       console.error("Erreur lors du fetch de l'utilisateur:", userRes?.error);
-  //     }
-  //   } catch (error) {
-  //     console.error("Erreur lors du fetch de l'utilisateur:", error);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   if (session.isAuthenticated && session.token) {
-  //     loadUser(session.token);
-  //   }
-  // }, [session]);
-
+  const upcomingFilteredEvents = useMemo(() => {
+    return events.filter((event) => event.isGoing || event.isFavourite);
+  }, [events]);
   return (
     <>
       {session.isAuthenticated ? (
