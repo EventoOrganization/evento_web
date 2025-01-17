4736b98be1a5d7521f044c492f5d3be6b8593cff
DUFOURNET Franck <franckdufournet@hotmail.fr>
2025-01-17 14:12:03 +0700

    test: test events feed

--- src/contexts/GlobalDataProvider.tsx
+++ src/contexts/GlobalDataProvider.tsx
@@ -11,13 +11,18 @@ const GlobalDataProvider: React.FC<{ children: React.ReactNode }> = ({
   const { user, token } = useSession();
 
   useEffect(() => {
+    console.log("User in GlobalDataProvider:", user); // DEBUG USER
+    console.log("Token in GlobalDataProvider:", token); // DEBUG TOKEN
+
     loadInterests();
     loadEvents(user || undefined);
 
     if (user?._id && token) {
+      console.log("Fetching user data for:", user._id);
       refreshUsers(user._id, token);
       loadUser(token);
     } else {
+      console.log("No user found, loading default users");
       loadUsers("", "");
     }
   }, [
