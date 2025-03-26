92896856c4c138bd9d9904c92473446eb155843f
DUFOURNET Franck <franckdufournet@hotmail.fr>
2025-03-26 14:35:18 +0700

    feat: add announcement question

--- src/features/event/update/AddUpdate.tsx
+++ src/features/event/update/AddUpdate.tsx
@@ -1,6 +1,8 @@
 import EventoSpinner from "@/components/EventoSpinner";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { RadioGroup } from "@/components/ui/radio-group";
 import {
   Select,
   SelectContent,
@@ -24,6 +26,15 @@ type AddAnnouncementProps = {
   setEvent?: (event: EventType) => void;
   selectedIds?: string[];
 };
+type DisplayType = "radio" | "checkbox" | "";
+type QuestionType = "text" | "multiple-choice" | "";
+
+type Question = {
+  question: string;
+  type: QuestionType;
+  displayType: DisplayType;
+  options: string[];
+};
 
 const AddAnnouncement = ({
   event,
@@ -37,17 +48,36 @@ const AddAnnouncement = ({
   const [isNotifying, setIsNotifying] = useState("");
   const [message, setMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [announcementType, setAnnouncementType] = useState<
+    "info" | "questionnaire"
+  >("info");
+  const [questions, setQuestions] = useState<Question[]>([
+    {
+      question: "",
+      type: "",
+      displayType: "",
+      options: [],
+    },
+  ]);
 
   const getPlaceholder = (value: string) => {
     switch (value) {
       case "going":
-        return "Notify all attendees who are going now!";
+        return announcementType === "info"
+          ? "Notify all attendees now!"
+          : "Notify all attendees to complete the questionnaire!";
       case "invited":
-        return "Notify all invited guests now!";
+        return announcementType === "info"
+          ? "Notify all invited guests now!"
+          : "Notify all invited guests to complete the questionnaire!";
       case "decline":
-        return "Notify all declined guests now!";
+        return announcementType === "info"
+          ? "Notify all declined guests now!"
+          : "Notify all declined guests to complete the questionnaire!";
       case "individuals":
-        return "Select people from the lists above to notify them.";
+        return announcementType === "info"
+          ? "Notify Selected people now!"
+          : "Notify Selected people to complete the questionnaire!";
       default:
         return "";
     }
@@ -77,6 +107,8 @@ const AddAnnouncement = ({
           userId: user?._id,
           message,
           receivers,
+          type: announcementType,
+          questions: announcementType === "questionnaire" ? questions : [],
         },
         token,
       );
@@ -151,35 +183,187 @@ const AddAnnouncement = ({
         </SelectContent>
       </Select>
       {isNotifying && (
-        <div className="col-span-2 flex flex-col gap-2">
+        <>
+          {/* <Label className="text-eventoPurpleLight">Announcement Type</Label> */}
+          <Select
+            value={announcementType}
+            onValueChange={(value) =>
+              setAnnouncementType(value as "info" | "questionnaire")
+            }
+          >
+            <SelectTrigger className="col-span-2">
+              <SelectValue placeholder="Select announcement type" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="info">Simple Announcement</SelectItem>
+              <SelectItem value="questionnaire">Questionnaire</SelectItem>
+            </SelectContent>
+          </Select>
           <Textarea
             placeholder={getPlaceholder(isNotifying)}
             value={message}
             onChange={(e) => setMessage(e.target.value)}
             disabled={isLoading}
+            className="col-span-2"
           />
-          <div className="flex w-fit gap-2 self-end">
-            <Button
-              disabled={isLoading}
-              onClick={() => {
-                setIsNotifying("");
-                setMessage("");
-                setIsSelectEnable(false);
-              }}
-              variant={"eventoSecondary"}
-            >
-              <XIcon size={16} />
-              Cancel
-            </Button>
-            <Button
-              variant={"eventoPrimary"}
-              onClick={handleAnnouncement}
-              disabled={isLoading}
-            >
-              {isLoading ? <EventoSpinner /> : "Send Announcement"}
-            </Button>
+          {announcementType === "questionnaire" && (
+            <div className="flex flex-col gap-4 col-span-2">
+              {questions.map((q, idx) => (
+                <div
+                  key={idx}
+                  className="border p-2 rounded-md bg-eventoPurpleDark"
+                >
+                  {" "}
+                  <Select
+                    value={
+                      q.type === "multiple-choice"
+                        ? q.displayType
+                        : q.type || ""
+                    }
+                    onValueChange={(val: "text" | "radio" | "checkbox") => {
+                      const updated = [...questions];
+                      if (val === "text") {
+                        updated[idx].type = "text";
+                        updated[idx].displayType = "";
+                        updated[idx].options = [];
+                      } else {
+                        updated[idx].type = "multiple-choice";
+                        updated[idx].displayType = val as "radio" | "checkbox";
+                        if (!updated[idx].options) {
+                          updated[idx].options = [""];
+                        }
+                      }
+                      setQuestions(updated);
+                    }}
+                  >
+                    <SelectTrigger className="">
+                      <SelectValue placeholder="Select answer type" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="text">Text (Free answer)</SelectItem>
+                      <SelectItem value="checkbox">
+                        Multiple choices (checkbox)
+                      </SelectItem>
+                      <SelectItem value="radio">
+                        Single choice (radio)
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <Input
+                    placeholder="Question text"
+                    value={q.question}
+                    onChange={(e) => {
+                      const updated = [...questions];
+                      updated[idx].question = e.target.value;
+                      setQuestions(updated);
+                    }}
+                  />
+                  {q.displayType === "checkbox" && (
+                    <div className="flex flex-col gap-2 mt-2">
+                      {q.options.map((opt, oIdx) => (
+                        <div key={oIdx} className="flex items-center gap-2">
+                          <Input
+                            value={opt}
+                            onChange={(e) => {
+                              const updated = [...questions];
+                              updated[idx].options[oIdx] = e.target.value;
+                              setQuestions(updated);
+                            }}
+                            placeholder={`Option ${oIdx + 1}`}
+                          />
+                        </div>
+                      ))}
+                      <Button
+                        variant="secondary"
+                        onClick={() => {
+                          const updated = [...questions];
+                          updated[idx].options.push("");
+                          setQuestions(updated);
+                        }}
+                      >
+                        + Add Option
+                      </Button>
+                    </div>
+                  )}
+                  {q.type === "multiple-choice" &&
+                    q.displayType === "radio" && (
+                      <RadioGroup
+                        value={""} // ici on ne sélectionne rien, juste pour afficher les options
+                        className="flex flex-col gap-2 mt-2"
+                      >
+                        {q.options.map((opt, oIdx) => (
+                          <div key={oIdx} className="flex items-center gap-2">
+                            <Input
+                              value={opt}
+                              onChange={(e) => {
+                                const updated = [...questions];
+                                updated[idx].options[oIdx] = e.target.value;
+                                setQuestions(updated);
+                              }}
+                              placeholder={`Option ${oIdx + 1}`}
+                              className="w-full"
+                            />
+                          </div>
+                        ))}
+
+                        <Button
+                          variant="secondary"
+                          onClick={() => {
+                            const updated = [...questions];
+                            updated[idx].options.push("");
+                            setQuestions(updated);
+                          }}
+                        >
+                          + Add Option
+                        </Button>
+                      </RadioGroup>
+                    )}
+                </div>
+              ))}
+
+              <Button
+                variant="outline"
+                onClick={() =>
+                  setQuestions([
+                    ...questions,
+                    {
+                      question: "",
+                      type: "text",
+                      displayType: "",
+                      options: [],
+                    },
+                  ])
+                }
+              >
+                + Add Question
+              </Button>
+            </div>
+          )}
+
+          <div className="col-span-2 flex flex-col gap-2">
+            <div className="flex w-fit gap-2 self-end">
+              <Button
+                disabled={isLoading}
+                onClick={() => {
+                  setIsNotifying("");
+                  setMessage("");
+                  setIsSelectEnable(false);
+                }}
+                variant={"eventoSecondary"}
+              >
+                <XIcon size={16} />
+                Cancel
+              </Button>
+              <Button
+                variant={"eventoPrimary"}
+                onClick={handleAnnouncement}
+                disabled={isLoading}
+              >
+                {isLoading ? <EventoSpinner /> : "Send Announcement"}
+              </Button>
+            </div>
           </div>
-        </div>
+        </>
       )}
     </>
   );
